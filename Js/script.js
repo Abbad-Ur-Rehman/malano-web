d036479678436c5047c4bdbce58ac20f03281ccb
Rehman <rasif650@gmail.com>
2023-08-10 23:49:16 +0500

    'first'

---
+++ Js/script.js
@@ -0,0 +1,22 @@
+// PAGE-ONE ELEMENTS
+let sideBarImgs = []
+
+sideBarImgs.push(document.getElementById('hooides'))
+sideBarImgs.push(document.getElementById('Sweatshirt'))
+sideBarImgs.push(document.getElementById('shirts'))
+sideBarImgs.push(document.getElementById('tshirts'))
+sideBarImgs.push(document.getElementById('jackets'))
+
+sideBarImgs.forEach((e)=>{
+    if(e.id === 'hooides'){
+        console.log(e)
+    }
+})
+
+
+// let arry = [1,2,3,4]
+// for (let index = 0; index < arry.length; index++) {
+//     const element = arry[2];
+//     console.log(element)
+    
+// }
